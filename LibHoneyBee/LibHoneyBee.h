a448c717a685ea14b335b5adda37d506b55c212e
Alex Lynch <alex@iamwebservices.com>
2017-01-13 09:37:20 -0500

    Create iOS framework target.

---
+++ LibHoneyBee/LibHoneyBee.h
@@ -0,0 +1,19 @@
+//
+//  LibHoneyBee.h
+//  LibHoneyBee
+//
+//  Created by Alex Lynch on 1/13/17.
+//  Copyright © 2017 IAM Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibHoneyBee.
+FOUNDATION_EXPORT double LibHoneyBeeVersionNumber;
+
+//! Project version string for LibHoneyBee.
+FOUNDATION_EXPORT const unsigned char LibHoneyBeeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibHoneyBee/PublicHeader.h>
+
+
---
+++ LibHoneyBee/LibHoneyBee.m
@@ -0,0 +1,13 @@
+//
+//  LibHoneyBee.m
+//  LibHoneyBee
+//
+//  Created by Alex Lynch on 1/13/17.
+//  Copyright © 2017 IAM Apps. All rights reserved.
+//
+
+#import "LibHoneyBee.h"
+
+@implementation LibHoneyBee
+
+@end
