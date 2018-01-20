0be72dd2c27624898d00bf809eae03d4a0d86db4
Alex Lynch <alex@iamwebservices.com>
2018-01-20 16:30:33 -0500

    Proper SPM support.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-	name: "HoneyBee"
+	name: "HoneyBee",
+	targets: [
+		.target(name: "HoneyBee",
+				dependencies: [],
+				path: "HoneyBee/"),
+		.testTarget(name: "HoneyBeeTests",
+					dependencies: ["HoneyBee"],
+					path: "HoneyBeeTests/"),
+		]
 )
