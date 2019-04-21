dc8eaf618901e49803ca01ece301d3b458d69597
Alex Lynch <alex@iamapps.net>
2019-04-21 16:33:36 -0400

    R publicize FailableResult as a typealias on Result<T, Error>

--- HoneyBee/FailableResult.swift
+++ HoneyBee/FailableResult.swift
@@ -20,7 +20,6 @@ public protocol FailableResultProtocol {
 	func value() throws -> Wrapped
 }
 
-#if swift(>=5.0)
 extension Result : FailableResultProtocol {
 	public func value() throws -> Success {
 		switch(self) {
@@ -31,37 +30,12 @@ extension Result : FailableResultProtocol {
 		}
 	}
 }
-#endif
 
-/// An enum containing the result of an operation which might fail.
-///
-/// - success: The operation was successful.
-/// - failure: The operation was not successful.
-/// - let.success:: The result T of the operation.
-/// - let.failure:: The error describing the failure.
-@available(swift, deprecated: 5.0, message: "Use Swift.Result instead")
-public enum FailableResult<T> : FailableResultProtocol {
-	/// - success: The operation was successful.
-	/// - let.success:: The result T of the operation.
-	case success(T)
-	/// - failure: The operation was not successful.
-	/// - let.failure:: The error describing the failure.
-	case failure(Swift.Error)
+public typealias FailableResult<T> = Result<T, Swift.Error>
+
+public extension Result where Failure == Swift.Error {
 	
-	/// If `self` is `.success` returns value of type `T`
-	/// Else if `self` is `.failure` throws error of
-	/// - Returns: The contained value
-	/// - Throws: An error if the value could not be returned.
-	public func value() throws -> T {
-		switch(self) {
-		case let .success(t) :
-			return t
-		case let .failure(error) :
-			throw error
-		}
-	}
-	
-	internal init<Failable>(_ failable: Failable) where Failable : FailableResultProtocol, Failable.Wrapped == T {
+	internal init<Failable>(_ failable: Failable) where Failable : FailableResultProtocol, Failable.Wrapped == Success {
 		do {
 			let t =  try failable.value()
 			self = .success(t)
@@ -71,7 +45,7 @@ public enum FailableResult<T> : FailableResultProtocol {
 	}
 	
 	/// implement functor API
-	public func map<R>(_ transform: (T) throws -> R) -> FailableResult<R> {
+	internal func map<R>(_ transform: (Wrapped) throws -> R) -> FailableResult<R> {
 		switch self {
 		case let .failure(error):
 			return .failure(error) // switch generic types
