0e466fa3a782876559dea21a447bb488b1ee3c74
Alex Lynch <lynchal@vmware.com>
2019-12-24 13:19:18 -0500

    Implement a non-HoneyBee solution to UploadPipleline test.

--- HoneyBeeTests/UploadPipeline.swift
+++ HoneyBeeTests/UploadPipeline.swift
@@ -13,7 +13,9 @@ class UploadPipeline: XCTestCase {
 
     private let exportLimit = 1
     private let uploadLimit = 4
-    private let uploadCount = 2
+    private let uploadCount = 20
+    private let uploadRetries = 1
+    private let acceptableFailureRate = 0.5
 
     private let concurrentExportCounter: AtomicInt = 0
     private let concurrentUploadCounter: AtomicInt = 0
@@ -22,6 +24,8 @@ class UploadPipeline: XCTestCase {
     private var totalExpectation: XCTestExpectation!
     private var singleUploadCompletionExpectation: XCTestExpectation!
     private var singleUploadSuccessExpectation: XCTestExpectation!
+
+    var mediaReferences: Array<String> { (1...self.uploadCount).map { "ref\($0)" } }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -48,8 +52,8 @@ class UploadPipeline: XCTestCase {
 
     /// An enum describing specific problems that the algorithm might encounter.
     enum UploadingError : Error {
-      case invalidResponse
-      case tooManyFailures
+        case invalidResponse
+        case tooManyFailures
     }
 
 
@@ -108,15 +112,112 @@ class UploadPipeline: XCTestCase {
         self.totalExpectation.fulfill()
     }
 
+    func testHoneyBee0() {
+
+        /// A semaphore to prevent flooding the NIC
+        let outerLimit = DispatchSemaphore(value: self.uploadLimit)
+        /// A semaphore to prevent thrashing the processor
+        let exportLimit = DispatchSemaphore(value: self.exportLimit)
+
+        /// How many of the uploads fully completed.
+        var uploadSuccesses = 0
+
+        // start in the background
+        DispatchQueue.global().async {
+            /// Dispatch group to keep track of when the entire process is finished
+            let fullProcessDispatchGroup: AtomicInt = 0
+            fullProcessDispatchGroup.guaranteeValueAtDeinit(0)
+
+            // this notify block is called when the full process has completed.
+            fullProcessDispatchGroup.notify {
+                DispatchQueue.main.async {
+                    let successRate = Double(uploadSuccesses) / Double(self.mediaReferences.count)
+                    if successRate > self.acceptableFailureRate {
+                        self.totalProcessSuccess()
+                    } else {
+                        self.errorHandler(UploadingError.tooManyFailures)
+                    }
+                }
+            }
+
+            for mediaRef in self.mediaReferences {
+                // alert the group that we're starting a process
+                fullProcessDispatchGroup.increment()
+                // wait until it's safe to start uploading
+                outerLimit.wait()
+
+                /// common cleanup operations needed later
+                func finalizeMediaRef() {
+                    self.singleUploadCompletion(mediaRef)
+                    fullProcessDispatchGroup.decrement()
+                    outerLimit.signal()
+                }
+
+                // wait until it's safe to start exporting
+                exportLimit.wait()
+                self.export(mediaRef) { (media, error) in
+                    // allow another export to begin
+                    exportLimit.signal()
+                    if let error = error {
+                        DispatchQueue.main.async {
+                            self.errorHandler(error)
+                            finalizeMediaRef()
+                        }
+                    } else {
+                        guard let media = media else {
+                            DispatchQueue.main.async {
+                                self.errorHandler(UploadingError.invalidResponse)
+                                finalizeMediaRef()
+                            }
+                            return
+                        }
+                        // the export was successful
+
+                        var uploadAttempts = 0
+                        /// define the upload process and its retry behavior
+                        func doUpload() {
+                            // respect Media's threading requirements
+                            self.managedObjectContext.asyncPerform {
+                                self.upload(media) { error in
+                                    if let error = error {
+                                        if uploadAttempts < self.uploadRetries {
+                                            uploadAttempts += 1
+                                            doUpload() // retry
+                                        } else {
+                                            DispatchQueue.main.async {
+                                                // too many upload failures
+                                                self.errorHandler(error)
+                                                finalizeMediaRef()
+                                            }
+                                        }
+                                    } else {
+                                        DispatchQueue.main.async {
+                                            uploadSuccesses += 1
+                                            self.singleUploadSuccess(media)
+                                            finalizeMediaRef()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        // kick off the first upload
+                        doUpload()
+                    }
+                }
+            }
+        }
+
+        self.waitForExpectations(timeout: 1500)
+    }
+
     func testHoneyBee2() {
-        let mediaReferences = (1...uploadCount).map { "ref\($0)" }
 
         let a = HoneyBee.start(on: DispatchQueue.main)
-                .handlingErrors(with: errorHandler)
-                .insert(mediaReferences)
+                        .handlingErrors(with: self.errorHandler)
+                        .insert(mediaReferences)
 
         let b = a.move(to: DispatchQueue.global())
-                .each(limit: uploadLimit, acceptableFailure: .ratio(0.5)) { elem in
+                .each(limit: uploadLimit, acceptableFailure: .ratio(self.acceptableFailureRate)) { elem in
                     elem.finally { link in
                         link.move(to: DispatchQueue.main)
                             .chain(self.singleUploadCompletion)
@@ -125,32 +226,33 @@ class UploadPipeline: XCTestCase {
                         link.chain(self.export)
                     }
                     .move(to: self.managedObjectContext)
-                    .retry(1) { link in
+                    .retry(self.uploadRetries) { link in
                         link.chain(self.upload) // subject to transient failure
                     }
                     .move(to: DispatchQueue.main)
                     .chain(self.singleUploadSuccess)
                     .move(to: DispatchQueue.global())
-                }.drop
+            }.drop
 
-                b.move(to: DispatchQueue.main)
-                .chain(totalProcessSuccess)
+            b.move(to: DispatchQueue.main)
+            .chain(totalProcessSuccess)
 
         self.waitForExpectations(timeout: 15)
     }
 
 
     func testHoneyBee3() {
-        let mediaReferences = (1...uploadCount).map { "ref\($0)" }
 
         let mainQ = HoneyBee.start(on: MainDispatchQueue())
-                            .handlingErrors(with: errorHandler)
+                            .handlingErrors(with: self.errorHandler)
 
         let backgroundQ = mainQ >> DefaultDispatchQueue()
 
         let asyncReferences = mediaReferences >> backgroundQ
 
-        let uploadComplete = asyncReferences.each(limit: uploadLimit, acceptableFailure: .ratio(0.5)) { reference -> Link<Void, MainDispatchQueue> in
+        let uploadComplete = asyncReferences.each(limit: uploadLimit,
+                                                  acceptableFailure: .ratio(self.acceptableFailureRate)) { reference -> Link<Void, MainDispatchQueue> in
+
             let completingRef = reference.finally { reference in
                 self.singleUploadCompletion(reference >> mainQ)
             }
@@ -160,7 +262,7 @@ class UploadPipeline: XCTestCase {
                 // the export is compute bound so limit it further
             }
 
-            let uploaded = media.retry(1) { media in
+            let uploaded = media.retry(self.uploadRetries) { media in
                 self.upload(media) +> media
                 // the upload is subject to transient failure
             }
